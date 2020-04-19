ad3dfc82b74a1927d83fd4e4a56b3e37b7b7739d
Jonathan-Mychack <60950786+Jonathan-Mychack@users.noreply.github.com>
2020-04-18 20:42:30 -0400

    Add files via upload

--- assign3/lcloud_filesys.c
+++ assign3/lcloud_filesys.c
@@ -16,6 +16,7 @@
 // Project include files
 #include <lcloud_filesys.h>
 #include <lcloud_controller.h>
+#include <lcloud_cache.h>
 
 //
 // File system interface implementation
@@ -25,15 +26,24 @@ typedef struct {
     int handle;
     int position;
     int length;
-    int device_id;
-    int blocks[40][2];
+    int blocks[40][3];  //order of indices is sector, block, device array index
 } File;
+
+typedef struct {
+    int num_sectors;
+    int num_blocks;
+    int id;
+    int **used_locations;
+} Device;
 
 //Variables
 File open_files_array[256];
+Device active_devices_array[16];
+int chosen_location[3];
+int active_devices[16] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
 int f_array_index = 0;
-
-int used_data_locations[LC_DEVICE_NUMBER_SECTORS][LC_DEVICE_NUMBER_BLOCKS];
+int handle = 1;
+int powered_on = 0;
 int sector = 0, block = 0;
 //
 
@@ -105,7 +115,7 @@ int power_on(void) {
     LCloudRegisterFrame rframe = lcloud_io_bus(frame, NULL);
     extract_lcloud_registers(rframe, &b0, &b1, &c0, &c1, &c2, &d0, &d1);
     if ((b0 != 1) || (b1 != 1) || (c0 != LC_POWER_ON)) {
-        return (-1);
+        return(-1);
     }
     return(0);
 }
@@ -118,7 +128,7 @@ int power_on(void) {
 // Inputs       : nothing
 //                
 //                
-// Outputs      : the device id if success, -1 if failure
+// Outputs      : 0 if success, -1 if failure
 int device_probe(void) {
     unsigned int b0, b1, c0, c1, c2, d0, d1;
     LCloudRegisterFrame frame = create_lcloud_registers(0, 0, LC_DEVPROBE, 0, 0, 0, 0);
@@ -129,16 +139,57 @@ int device_probe(void) {
     }
     
     int bit = 0;
+    int active_devices_index = 0;
     while (d0 > 0) {
         int remainder = d0 % 2;
         d0 /= 2;
         if (remainder == 1) {
-            return(bit);
+            active_devices[active_devices_index] = bit;
+            active_devices_index += 1;
         }
         bit += 1;
     }
 
-    return(-1);
+    return(0);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Function     : device_init
+// Description  : finds the number or sectors and blocks for each device and 
+//                dynamically allocates a space to record which locations are available for use
+// Inputs       : nothing
+//                
+//                
+// Outputs      : 0 if success, -1 if failure
+int device_init(void) {
+    unsigned int b0, b1, c0, c1, c2, d0, d1;
+    int index = 0;
+    for (int id = 0; id < 16; id++) {
+
+        if (active_devices[id] != -1) {
+
+            LCloudRegisterFrame frame = create_lcloud_registers(0, 0, LC_DEVINIT, active_devices[id], 0, 0, 0);
+            LCloudRegisterFrame rframe = lcloud_io_bus(frame, NULL);
+            extract_lcloud_registers(rframe, &b0, &b1, &c0, &c1, &c2, &d0, &d1);
+            if ((b0 != 1) || (b1 != 1) || (c0 != LC_DEVINIT)) {
+                return(-1);
+            }
+
+            active_devices_array[index].used_locations = (int**)calloc(d0, sizeof(int*));
+            for (int i = 0; i < d0; i++) {
+                active_devices_array[index].used_locations[i] = (int*)calloc(d1, sizeof(int));
+            }
+
+            active_devices_array[index].num_sectors = d0;
+            active_devices_array[index].num_blocks = d1;
+            active_devices_array[index].id = active_devices[id];
+            index += 1;
+
+        }
+    }
+
+    return(0);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -185,6 +236,40 @@ int put_block(char *buffer, int device_id, int sector, int block) {
 
 ////////////////////////////////////////////////////////////////////////////////
 //
+// Function     : choose_location
+// Description  : selects a sector and block from a device to be used for writing
+//
+// Inputs       : nothing
+//                
+//                
+// Outputs      : 0 if success, -1 if failure
+int choose_location(void) {
+
+    for (int device = 0; device < 15; device++) {
+
+        if (active_devices_array[device].id != NULL) {
+
+            for (int sector = 0; sector < active_devices_array[device].num_sectors; sector++) {
+
+                for (int block = 0; block < active_devices_array[device].num_blocks; block++) {
+
+                    if (active_devices_array[device].used_locations[sector][block] == 0) {
+
+                        chosen_location[0] = sector;
+                        chosen_location[1] = block;
+                        chosen_location[2] = device;
+                        return(0);
+                    }
+                }
+            }
+        }
+    }
+
+    return(-1);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
 // Function     : lcopen
 // Description  : Open the file for for reading and writing
 //
@@ -192,8 +277,14 @@ int put_block(char *buffer, int device_id, int sector, int block) {
 // Outputs      : file handle if successful test, -1 if failure
 
 LcFHandle lcopen( const char *path ) {
-    power_on();
-    int device_id = device_probe();
+
+    if (powered_on == 0) {
+        power_on();
+        device_probe();
+        device_init();
+        lcloud_initcache(LC_CACHE_MAXBLOCKS);
+        powered_on = 1;
+    }
 
 
 
@@ -205,17 +296,17 @@ LcFHandle lcopen( const char *path ) {
 
 
 
-    int handle = 1;
     strcpy(open_files_array[f_array_index].filename, path);
     open_files_array[f_array_index].handle = handle;
     open_files_array[f_array_index].position = 0;
     open_files_array[f_array_index].length = 0;
-    open_files_array[f_array_index].device_id = device_id;
     for (int i = 0; i < 40; i++) {
         open_files_array[f_array_index].blocks[i][0] = -1;
         open_files_array[f_array_index].blocks[i][1] = -1;
+        open_files_array[f_array_index].blocks[i][2] = -1;
     }
     f_array_index += 1;
+    handle += 1;
 
 
 
@@ -235,7 +326,6 @@ int lcread( LcFHandle fh, char *buf, size_t len ) {
 
     int open;
     int location;
-    char buffer[256];
 
 
 
@@ -244,6 +334,7 @@ int lcread( LcFHandle fh, char *buf, size_t len ) {
         if (open_files_array[i].handle == fh) {
             open = 1;
             location = i;
+            break;
         }
     }
 
@@ -252,7 +343,7 @@ int lcread( LcFHandle fh, char *buf, size_t len ) {
     }
 
     if (len > open_files_array[location].length) {  //check if operation size is over the size of the file
-        return(-1);
+        len = open_files_array[location].length;
     }
 
     
@@ -269,18 +360,35 @@ int lcread( LcFHandle fh, char *buf, size_t len ) {
 
     for (int read = 0; read < total_possible_reads; read++) {
 
+        char buffer[256] = {0};
+        //char *cache_buffer;
+
         int section = open_files_array[location].position / 256;  //use the markers in the file struct to determine which sector and block the current position is in
         int temp_sector = open_files_array[location].blocks[section][0];
         int temp_block = open_files_array[location].blocks[section][1];
+        int device_index = open_files_array[location].blocks[section][2];
         int index = open_files_array[location].position % 256;  //starting index to be used for the buffer that reads the data
         int block_space_remaining = 256 - index;
 
-        get_block(buffer, open_files_array[location].device_id, temp_sector, temp_block);
-        logMessage(LcDriverLLevel, "Success reading blkc [%d/%d/%d].", open_files_array[location].device_id, temp_sector, temp_block);
+        /*cache_buffer = lcloud_getcache(active_devices_array[device_index].id, temp_sector, temp_block);
+
+        if (cache_buffer == '-1') {
+            get_block(buffer, active_devices_array[device_index].id, temp_sector, temp_block);
+            lcloud_putcache(active_devices_array[device_index].id, temp_sector, temp_block, buffer);
+        }
+        else {
+
+            for (int index = 0; index < 256; index++) {
+
+                buffer[index] = cache_buffer[index];
+            }
+        }*/
+        get_block(buffer, active_devices_array[device_index].id, temp_sector, temp_block);
+        logMessage(LcDriverLLevel, "Success reading blkc [%d/%d/%d].", active_devices_array[device_index].id, temp_sector, temp_block);
 
         if ((len - count) <= block_space_remaining) {  //if the current read can be done without exceeding the block space
-
-            for (int i = 0; i < (len - count); i++) {
+            int temp = len - count;
+            for (int i = 0; i < temp; i++) {
 
                 buf[count++] = buffer[index + i];
                 open_files_array[location].position += 1;
@@ -318,7 +426,6 @@ int lcwrite( LcFHandle fh, char *buf, size_t len ) {
 
     int open;
     int location;
-    char buffer[256];
 
 
 
@@ -327,6 +434,7 @@ int lcwrite( LcFHandle fh, char *buf, size_t len ) {
         if (open_files_array[i].handle == fh) {
             open = 1;
             location = i;
+            break;
         }
     }
 
@@ -346,6 +454,7 @@ int lcwrite( LcFHandle fh, char *buf, size_t len ) {
     int bytes_in_block = 0;
     int write_size;
     int overwrite = 0;  //marker for whether an overwrite is desired
+    int device_index;
 
     if (open_files_array[location].position != open_files_array[location].length) {
 
@@ -354,21 +463,38 @@ int lcwrite( LcFHandle fh, char *buf, size_t len ) {
 
     for (int write = 0; write < total_possible_writes; write++) {
 
-        sector = open_files_array[location].position / 2560;  //changes sectors every 10 blocks in order to mimic the sample output
-        block = (open_files_array[location].position / 256) - (sector * 10);  //uses 10 sectors then resets to 0
+        char buffer[256] = {0};
 
-        if ((open_files_array[location].position % 256) == 0) {  //if a new block is selected, give a notification about its allocation
+        if (((open_files_array[location].position % 256) == 0) && (overwrite == 0)) {  //select a new location to write into if the current block has been filled
             
-            logMessage(LcDriverLLevel, "Allocated block for data [%d/%d/%d]", open_files_array[location].device_id, sector, block);
+            choose_location();
+            sector = chosen_location[0];
+            block = chosen_location[1];
+            device_index = chosen_location[2];
+
+            logMessage(LcDriverLLevel, "Allocated block for data [%d/%d/%d]", active_devices_array[device_index].id, sector, block);
+        }
+        else {//if (overwrite == 1) { //if an overwrite is necessary, go to the location where the overwrite is occurring
+
+            int section = open_files_array[location].position / 256;
+            sector = open_files_array[location].blocks[section][0];
+            block = open_files_array[location].blocks[section][1];
+            device_index = open_files_array[location].blocks[section][2];
         }
     
-        if (((open_files_array[location].position % 256) != 0) && (overwrite == 0)) {  //if the position is in the middle of a block
+        if (((open_files_array[location].position % 256) != 0)) {  //if the position is in the middle of a block
 
             int temp_pos = open_files_array[location].position;
             int read_pos = open_files_array[location].position - (open_files_array[location].position % 256);  //the position of the beginning of the current block
             lcseek(fh, read_pos);
+
             bytes_in_block = lcread(fh, buffer, temp_pos % 256);  //read up until the latest byte
             int block_space_remaining = 256 - bytes_in_block;
+            if (overwrite == 1) {
+                open_files_array[location].position = read_pos;
+                lcread(fh, buffer, 256);
+                open_files_array[location].position = temp_pos;
+            }
 
             if ((len - count) <= block_space_remaining) {  //if the current write can fit in the rest of the block
 
@@ -379,9 +505,14 @@ int lcwrite( LcFHandle fh, char *buf, size_t len ) {
                 write_size = block_space_remaining;
             }
         }
-        else {  //if the position is at the beginning of a block
+        else if (((open_files_array[location].position % 256) == 0)) {  //if the position is at the beginning of a block
 
             bytes_in_block = 0;
+            if (overwrite == 1) {
+                int temp_pos = open_files_array[location].position;
+                lcread(fh, buffer, 256);
+                open_files_array[location].position = temp_pos;
+            }
 
             if ((len - count) <= 256) {  //if the current write can fit in the entire block
 
@@ -394,22 +525,30 @@ int lcwrite( LcFHandle fh, char *buf, size_t len ) {
         }
 
         memcpy(&buffer[bytes_in_block], &buf[count], write_size);
-        put_block(buffer, open_files_array[location].device_id, sector, block);
+        //lcloud_putcache(active_devices_array[device_index].id, sector, block, buffer);
+        put_block(buffer, active_devices_array[device_index].id, sector, block);
 
         int section = open_files_array[location].length / 256;  //make note of which sector and block was used for this part of the file
-        open_files_array[location].blocks[section][0] = sector;
-        open_files_array[location].blocks[section][1] = block;
-        if (used_data_locations[sector][block] == 0) {  //mark this sector and block as used if it hasn't been already
-            used_data_locations[sector][block] = 1;
+        if (open_files_array[location].blocks[section][0] == -1) {
+            open_files_array[location].blocks[section][0] = sector;
+            open_files_array[location].blocks[section][1] = block;
+            open_files_array[location].blocks[section][2] = device_index;
+        }
+        if (active_devices_array[device_index].used_locations[sector][block] == 0) {  //mark this sector and block as used if it hasn't been already
+            active_devices_array[device_index].used_locations[sector][block] = 1;
         }
 
         count += write_size;
         if (open_files_array[location].position == open_files_array[location].length) {  //only change the length if writing after current length of the file
             open_files_array[location].length += write_size;
         }
+        else if ((overwrite == 1) && ((open_files_array[location].position + write_size) > open_files_array[location].length)) {
+            int difference = open_files_array[location].length - open_files_array[location].position;
+            open_files_array[location].length += write_size - difference;
+        }
         open_files_array[location].position += write_size;
 
-        logMessage(LcDriverLLevel, "LC success writing blkc [%d/%d/%d].", open_files_array[location].device_id, sector, block);
+        logMessage(LcDriverLLevel, "LC success writing blkc [%d/%d/%d].", active_devices_array[device_index].id, sector, block);
 
         if (count == len) {
 
@@ -429,7 +568,7 @@ int lcwrite( LcFHandle fh, char *buf, size_t len ) {
 //
 // Inputs       : fh - the file handle of the file to seek in
 //                off - offset within the file to seek to
-// Outputs      : 0 if successful test, -1 if failure
+// Outputs      : the file position if successful, -1 if failure
 
 int lcseek( LcFHandle fh, size_t off ) {
     
@@ -452,7 +591,7 @@ int lcseek( LcFHandle fh, size_t off ) {
 
     logMessage(LcDriverLLevel, "Seeking to position %d in file handle %d [%s]", off, open_files_array[location].handle, open_files_array[location].filename);
     open_files_array[location].position = off;
-    return( 0 );
+    return(open_files_array[location].position);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -481,8 +620,14 @@ int lcclose( LcFHandle fh ) {
     open_files_array[location].position = 0;
     for (int i = 0; i < 40; i++) {
         if (open_files_array[location].blocks[i][0] != -1) {
-            used_data_locations[open_files_array[location].blocks[i][0]][open_files_array[location].blocks[i][1]] = 0;
-            logMessage(LcDriverLLevel, "Deallocated block for data [%d/%d/%d]", open_files_array[location].device_id, open_files_array[location].blocks[i][0], open_files_array[location].blocks[i][1]);
+            int device_id = active_devices_array[open_files_array[location].blocks[i][2]].id;
+            for (int device = 0; device < 16; device++) {
+                if (active_devices_array[device].id == device_id) {
+                    active_devices_array[device].used_locations[open_files_array[location].blocks[i][0]][open_files_array[location].blocks[i][1]] = 0;
+                }
+            }
+
+            logMessage(LcDriverLLevel, "Deallocated block for data [%d/%d/%d]", open_files_array[location].blocks[i][2], open_files_array[location].blocks[i][0], open_files_array[location].blocks[i][1]);
         }
         open_files_array[location].blocks[i][0] = -1;
         open_files_array[location].blocks[i][1] = -1;
@@ -509,6 +654,14 @@ int lcshutdown( void ) {
     if ((b0 != 1) || (b1 != 1) || (c0 != LC_POWER_OFF)) {
         return (-1);
     }
+
+    for (int device = 0; device < 16; device++) {
+        for (int sector = 0; sector < active_devices_array[device].num_sectors; sector++) {
+            free(active_devices_array[device].used_locations[sector]);
+        }
+        free(active_devices_array[device].used_locations);
+    }
+
     logMessage(LcDriverLLevel, "Powered off the LionCloud system.");
     return(0);
 }
